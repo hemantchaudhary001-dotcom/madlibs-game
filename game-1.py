0464574eb67b62a7ca6f3ebab4a2479f4f580144
Hemant Chaudhary <hemantchaudhry001@gmail.com>
2025-09-21 23:43:22 +0545

    Add files via upload

---
+++ game-1.py
@@ -0,0 +1,19 @@
+#madlibs game
+
+place = input("enter a place:")
+noun = input("enter a noun:")
+adjective = input("enter an adjective:")
+another_place = input("enter a place:")
+food = input("enter a food:")
+another_noun = input("enter another noun:")
+another_adjective = input("enter another adjective:")
+
+
+
+print(f"today i went to {place} and i saw my  {noun} on the way i was very {adjective} to see them.")
+print(f"we went to {another_place} and had a lot of fun. then we ate {food} for lunch.")
+print(f"after that we went to see the {another_noun} and it was very {another_adjective}.")
+print("it was a great day!")
+print("the end")
+print("thank you for playing my game!") 
+ 
